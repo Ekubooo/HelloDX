1313b62acd0d9e8e8458874aa78c5c1bae62d698
PepperShaker <martherssteve@gmail.com>
2025-06-27 18:36:34 +0800

    update DX framework

--- xmake.lua
+++ xmake.lua
@@ -1,75 +1,35 @@
 add_rules("mode.debug", "mode.release")
+set_defaultmode("debug")
+set_toolchains("msvc")
+set_languages("c99", "cxx17")
+set_encodings("utf-8")
 
-target("HelloDX")
+if is_os("windows") then 
+    add_defines("UNICODE")
+    add_defines("_UNICODE")
+end
+
+function add_dx_sdk_options()
+    if has_config("WIN7_SYSTEM_SUPPORT") then
+        add_defines("_WIN32_WINNT=0x601")
+    end
+    add_syslinks("d3d11","dxgi","dxguid","D3DCompiler","d2d1","dwrite","winmm","user32","gdi32","ole32")
+end
+
+includes("scripts.lua")
+--ImGui
+includes("ImGui")
+-- Assimp
+add_requires("assimp",{system = false })
+
+-- ////////////////
+targetName = "HelloDX"
+target(targetName)
+    set_group("Project 01-09")
     set_kind("binary")
-    add_files("src/*.cpp")
-
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro definition
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
-
+    set_targetdir(path.join(binDir,targetName))
+    add_dx_sdk_options()
+    add_headerfiles("**.h")
+    add_files("**.cpp")
+target_end()
+-- ////////////////
